7c65f575ac965cf2d500a8e6a680ee4503b388ff
Keith Rocheck <749812+krocheck@users.noreply.github.com>
2020-04-13 15:16:06 -0400

    suppress cont and sat vars for smView4K

--- variables.js
+++ variables.js
@@ -10,21 +10,27 @@ module.exports = {
 		var variables = [];
 
 		variables.push({ label: 'Monitor A Brightness', name: 'mon_a_brightness' });
-		variables.push({ label: 'Monitor A Contrast',   name: 'mon_a_contrast' });
-		variables.push({ label: 'Monitor A Saturation', name: 'mon_a_saturation' });
-
 		this.setVariable('mon_a_brightness', this.getMonitor('A').brightness);
-		this.setVariable('mon_a_contrast',   this.getMonitor('A').contrast);
-		this.setVariable('mon_a_saturation', this.getMonitor('A').saturation);
+
+		if ( this.config.ver != 'smView4K' ) {
+			variables.push({ label: 'Monitor A Contrast',   name: 'mon_a_contrast' });
+			variables.push({ label: 'Monitor A Saturation', name: 'mon_a_saturation' });
+
+			this.setVariable('mon_a_contrast',   this.getMonitor('A').contrast);
+			this.setVariable('mon_a_saturation', this.getMonitor('A').saturation);
+			}
 
 		if ( this.config.ver == 'smViewDuo' || this.config.ver == 'smScope' ) {
 			variables.push({ label: 'Monitor B Brightness', name: 'mon_b_brightness' });
-			variables.push({ label: 'Monitor B Contrast',   name: 'mon_b_contrast' });
-			variables.push({ label: 'Monitor B Saturation', name: 'mon_b_saturation' });
-
 			this.setVariable('mon_b_brightness', this.getMonitor('B').brightness);
-			this.setVariable('mon_b_contrast',   this.getMonitor('B').contrast);
-			this.setVariable('mon_b_saturation', this.getMonitor('B').saturation);
+
+			if ( this.config.ver != 'smView4K' ) {
+				variables.push({ label: 'Monitor B Contrast',   name: 'mon_b_contrast' });
+				variables.push({ label: 'Monitor B Saturation', name: 'mon_b_saturation' });
+
+				this.setVariable('mon_b_contrast',   this.getMonitor('B').contrast);
+				this.setVariable('mon_b_saturation', this.getMonitor('B').saturation);
+			}
 		}
 
 		this.setVariableDefinitions(variables);
