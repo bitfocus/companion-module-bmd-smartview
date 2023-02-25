087d70e4c7705a1237fb2d8c9bd5e877cbc29d67
Keith Rocheck <749812+krocheck@users.noreply.github.com>
2023-02-25 11:09:56 -0500

    move receiveBuffer to this

--- src/index.js
+++ src/index.js
@@ -181,7 +181,7 @@ class BlackmagicSmartviewInstance extends InstanceBase {
 	 * @since 1.0.0
 	 */
 	initTCP() {
-		var receivebuffer = ''
+		this.receiveBuffer = ''
 
 		if (this.socket !== undefined) {
 			this.socket.destroy()
@@ -212,10 +212,10 @@ class BlackmagicSmartviewInstance extends InstanceBase {
 				let i = 0,
 					line = '',
 					offset = 0
-				receivebuffer += chunk
+				this.receiveBuffer += chunk
 
-				while ((i = receivebuffer.indexOf('\n', offset)) !== -1) {
-					line = receivebuffer.substr(offset, i - offset)
+				while ((i = this.receiveBuffer.indexOf('\n', offset)) !== -1) {
+					line = this.receiveBuffer.substr(offset, i - offset)
 					offset = i + 1
 
 					if (line.match(/Error/)) {
@@ -238,7 +238,7 @@ class BlackmagicSmartviewInstance extends InstanceBase {
 					}
 				}
 
-				receivebuffer = receivebuffer.substr(offset)
+				this.receiveBuffer = this.receiveBuffer.substr(offset)
 			})
 
 			this.socket.on('receiveline', (line) => {
